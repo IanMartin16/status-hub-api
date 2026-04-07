c8e5a4a2a67cca14fe252583a6d5ae045462d3d4
ianmartin16 <imartindm20@gmail.com>
2026-04-06 22:14:30 -0600

    Fixed: curpify domain

--- app/main.py
+++ app/main.py
@@ -54,7 +54,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "https://www.curpify.com/"
+        "https://www.curpify.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
