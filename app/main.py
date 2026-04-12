f985ab2aed40425c2f1e7a0be8947a2aea67c573
ianmartin16 <imartindm20@gmail.com>
2026-04-12 14:56:58 -0600

    Add: add evilink domain to cors

--- app/main.py
+++ app/main.py
@@ -55,6 +55,8 @@ app.add_middleware(
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "https://www.curpify.com"
+        "https://evilink.dev"
+        "https://www.evilink.dev"
     ],
     allow_credentials=True,
     allow_methods=["*"],
